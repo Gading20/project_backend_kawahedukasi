f87eb0c107a86505c46a2c9a8275aabf6dcf42d4
Gading20 <96716856+Gading20@users.noreply.github.com>
2022-07-13 14:37:04 +0700

    Add files via upload

---
+++ soal3.java
@@ -0,0 +1,70 @@
+public class soal3 {
+     public String nama;
+     public String jumlah_kehadiran;
+     public String status_kelulusan;
+
+     public String status_pekerjaan;
+     public int gaji;
+     public int id;
+     public int nilai;
+
+     public soal3(){
+
+     }
+
+     public String getNama() {
+          return nama;
+     }
+
+     public void setNama(String nama) {
+          this.nama = nama;
+     }
+
+     public String getJumlah_kehadiran() {
+          return jumlah_kehadiran;
+     }
+
+     public void setJumlah_kehadiran(String jumlah_kehadiran) {
+          this.jumlah_kehadiran = jumlah_kehadiran;
+     }
+
+     public String getStatus_kelulusan() {
+          return status_kelulusan;
+     }
+
+     public void setStatus_kelulusan(String status_kelulusan) {
+          this.status_kelulusan = status_kelulusan;
+     }
+
+     public String getStatus_pekerjaan() {
+          return status_pekerjaan;
+     }
+
+     public void setStatus_pekerjaan(String status_pekerjaan) {
+          this.status_pekerjaan = status_pekerjaan;
+     }
+
+     public int getGaji() {
+          return gaji;
+     }
+
+     public void setGaji(int gaji) {
+          this.gaji = gaji;
+     }
+
+     public int getId() {
+          return id;
+     }
+
+     public void setId(int id) {
+          this.id = id;
+     }
+
+     public int getNilai() {
+          return nilai;
+     }
+
+     public void setNilai(int nilai) {
+          this.nilai = nilai;
+     }
+}
---
+++ test_soal2.java
@@ -0,0 +1,10 @@
+public class test_soal2 {
+    public static void main(String[] args) {
+        String[] nama = {"ka", "wah", "edukasi"};
+        int[] angka = {1,67,1772,7,98,-1};
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println("indeks ke :"+i+" "+ nama[i]+" "+angka[i]);
+            System.out.println("[1,kawah,0]");
+        }
+    }
+}
